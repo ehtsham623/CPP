06e74d2800f500aede1ae0a37edd92f44bef748a
ehtsham623 <ehtshamulhaqat@gmail.com>
2024-11-06 13:53:41 +0500

    butterfly

---
+++ learningC/patterns/butterfly.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void method()
+{
+}
+
+int main()
+{
+
+    method();
+
+    return 0;
+}
+
+// to run add below line in terminal
+// g++ butterfly.cpp && ./a.out
